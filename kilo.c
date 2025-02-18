288c42e3ee4943a9f1ac813eb7b33f529d2bd3e5
Skytlz <skytlzdev@gmail.com>
2025-02-18 11:55:29 -0500

    04 - Limit Scrolling to the right

--- kilo.c
+++ kilo.c
@@ -284,6 +284,8 @@ void editorRefreshScreen() {
 
 /*** Input ***/
 void editorMoveCursor(int key) {
+    erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+
     switch (key) {
         case ARROW_LEFT:
             if (E.cx != 0) {
@@ -291,7 +293,9 @@ void editorMoveCursor(int key) {
             }
             break;
         case ARROW_RIGHT:
+            if (row && E.cx < row->size) {
                 E.cx++;
+            }
             break;
         case ARROW_UP:
             if (E.cy != 0) {
